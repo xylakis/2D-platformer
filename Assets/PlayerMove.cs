26dd45ebd8b09ab46293dc8052e80bd10eda4bbd
xylakis <emmanouil.xylakis@gmail.com>
2025-04-24 23:31:51 +0300

    Unity files

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+
+    float horizontalMovement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontalMovement = context.ReadValue<Vector2>().x;
+        Debug.Log(horizontalMovement);
+    }
+
+}
