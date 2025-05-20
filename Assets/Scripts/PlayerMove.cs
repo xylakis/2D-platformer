bb0a2ad1a2da8310881ee8eb049f7480ffa210f2
Suxumuxu <emmanouil.xylakis@gmail.com>
2025-05-20 17:09:57 +0300

    flip

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -8,6 +8,8 @@ public class PlayerMove : MonoBehaviour
 {
 
     public Rigidbody2D rb;
+    bool isFacingRight  = true;
+    
     [Header("Movement")]
     public float moveSpeed = 5f;
     float horizontalMovement;
@@ -22,11 +24,18 @@ public class PlayerMove : MonoBehaviour
     public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
     public LayerMask groundLayer;
 
+    [Header("WallCheck")]
+    public Transform wallCheckPos;
+    public Vector2 wallCheckSize = new Vector2(0.5f, 0.05f);
+    public LayerMask wallLayer;
+
     // Update is called once per frame
     void Update()
     {
         rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
         GroundCheck();
+
+        Flip();
     }
 
     public void Move(InputAction.CallbackContext context)
@@ -59,11 +68,29 @@ public class PlayerMove : MonoBehaviour
         {
             jumpsRemaining = maxJumps;
         }
+
+        if (Physics2D.OverlapBox(wallCheckPos.position, wallCheckSize, 0, wallLayer))
+        {
+            jumpsRemaining = 1;
+        }
     } 
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontalMovement <0 || !isFacingRight && horizontalMovement > 0)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
+    }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawCube(groundCheckPos.position, groundCheckSize);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawCube(wallCheckPos.position, wallCheckSize);
     }
 }
